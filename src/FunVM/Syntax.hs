b6fbdea265738de9018f5eefeecc7def299c6dde
Tom Lokhorst <tom@lokhorst.eu>
2010-11-05 16:12:56 +0100

    Merged Types module into Syntax. FunVM.Syntax now matches "Strictness Optimization for Higher-Order Functions in a Typed Intermediate Language" paper.

--- src/FunVM/Syntax.hs
+++ src/FunVM/Syntax.hs
@@ -1,20 +1,90 @@
 module FunVM.Syntax
   ( Id
+  , TyVar
+  , Types
+  , Type (..)
+  , Base (..)
+  , Kind (..)
+  , Bind (..)
+  , ValBind (..)
+  , Val (..)
+  , Literal (..)
+  , Expr (..)
   , Group
   , Module (..)
-  , Bind (..)
-  , Pat (..)
-  , Val (..)
-  , Expr (..)
-  , Literal (..)
   , fv
   ) where
 
 import Data.List
-import FunVM.Types
 
 -- | Identifier for variables and other names
 type Id = String
+
+-- | Type Variable
+type TyVar = String
+
+-- | List of types
+type Types = [Type]
+
+-- | Kind of a type
+data Kind
+  = Star
+  deriving Eq
+
+-- | Binding patterns in lambdas, lets or top level bindings
+data Bind
+  = TermPat  Id  Type
+  | TypePat  Id  Kind
+  deriving Eq
+
+-- | Single type
+data Type
+  = Base    Base
+  | TyVar   TyVar
+  | Fun     [Bind]  Types
+  | Lazy    Types
+  deriving Eq
+
+-- | Built-in types
+data Base
+  = Int { bitSize :: Int }
+  | Float32
+  | Double64
+  | Character
+  | Utf16String
+  deriving Eq
+
+-- Value binding
+data ValBind
+  = Bind Bind Val
+  deriving Eq
+
+-- | Values in Weak Head Normal Form
+data Val
+  = Lit    Literal 
+  | Lam    [Bind]  Expr
+  | Delay  Expr
+  deriving Eq
+
+-- | Main expression data type
+data Expr
+  = Val     Val
+  | Var     Id
+  | App     Expr    Expr
+  | Multi   [Expr]
+  | Force   Expr
+  | Let     [Bind]   Expr  Expr
+  | LetRec  [ValBind]  Expr
+  | FFI     String  Type
+  deriving Eq
+
+-- | Literal integers, characters or strings
+data Literal
+  = Integer  Integer  Type
+  | Char     Char
+  | String   String
+  | Type     Type
+  deriving Eq
 
 -- | Group of mutually dependent bindings
 type Group = [Bind]
@@ -28,44 +98,6 @@ data Module
       }
   deriving Eq
 
--- Value binding
-data Bind
-  = Bind Pat Val
-  deriving Eq
-
--- | Patterns in lambdas, lets or top level bindings
-data Pat
-  = TermPat  Id  Type
-  | TypePat  Id  Kind
-  deriving Eq
-
--- | Values in Weak Head Normal Form
-data Val
-  = Lit    Literal 
-  | Lam    [Pat]  Expr
-  | Delay  Expr
-  deriving Eq
-
--- | Main expression data type
-data Expr
-  = Val     Val
-  | Var     Id
-  | App     Expr    Expr
-  | Multi   [Expr]
-  | Force   Expr
-  | Let     [Pat]   Expr  Expr
-  | LetRec  [Bind]  Expr
-  | FFI     String  Type
-  deriving Eq
-
--- | Literal integers, characters or strings
-data Literal
-  = Integer  Integer  Type
-  | Char     Char
-  | String   String
-  | Type     Type
-  deriving Eq
-
 -- | Free value variables in expression
 fv :: Expr -> [Id]
 fv (Val (Lit     _))    = []
@@ -76,23 +108,23 @@ fv (App     e1 e2)      = fv e1 `union` fv e2
 fv (Force   e)          = fv e
 fv (Multi   es)         = concatMap fv es
 fv (Let     ps e1 e2)   = (fv e2 \\ map patId ps) `union` fv e1
-fv (LetRec  bs e)       = (foldr union (fv e) (map (val (const []) (const fv) fv . bindVal) bs))
-                            \\ map (patId . bindPat) bs
+fv (LetRec  bs e)       = (foldr union (fv e) (map (val (const []) (const fv) fv . valBindVal) bs))
+                            \\ map (patId . valBindBind) bs
 fv (FFI     _ _)        = []
 
 -- Small helper functions
 
-patId :: Pat -> Id
+patId :: Bind -> Id
 patId (TermPat x _) = x
 patId (TypePat x _) = x
 
-bindPat :: Bind -> Pat
-bindPat (Bind p _) = p
+valBindBind :: ValBind -> Bind
+valBindBind (Bind p _) = p
 
-bindVal :: Bind -> Val
-bindVal (Bind _ v) = v
+valBindVal :: ValBind -> Val
+valBindVal (Bind _ v) = v
 
-val :: (Literal -> a) -> ([Pat] -> Expr -> a) -> (Expr -> a) -> Val -> a
+val :: (Literal -> a) -> ([Bind] -> Expr -> a) -> (Expr -> a) -> Val -> a
 val f _ _ (Lit l)    = f l
 val _ g _ (Lam ps e) = g ps e
 val _ _ h (Delay e)  = h e
