c23472ce33057a0ab3ba0ce25c3efbdc07e7a0d5
Tom Lokhorst <tom@lokhorst.eu>
2010-11-07 19:09:04 +0100

    Added fib test function.

--- src/Test.hs
+++ src/Test.hs
@@ -1,4 +1,4 @@
-module Test where
+module Main where
 
 import FunVM.Build
 import FunVM.Evaluator
@@ -97,13 +97,13 @@ test2 = lets [ ( [TermPat "add" arith]
                     ])
 
 test3 :: Expr
-test3 = lets [ fun "const" [ TypePat "a" Star
-                           , TypePat "b" Star
-                           , TermPat "x" $ Lazy [TyVar "a"]
-                           , TermPat "y" $ Lazy [TyVar "b"]
-                           ]
-                           [TyVar "a"]
-                           (Force $ Var "x")
+test3 = lets [ fn "const" [ TypePat "a" Star
+                          , TypePat "b" Star
+                          , TermPat "x" $ Lazy [TyVar "a"]
+                          , TermPat "y" $ Lazy [TyVar "b"]
+                          ]
+                          [TyVar "a"]
+                          (Force $ Var "x")
              , ( [TermPat "add" arith]
                , (Val $ Prim "primAddInt32" arith)
                )
@@ -114,6 +114,49 @@ test3 = lets [ fun "const" [ TypePat "a" Star
                              , Val $ Delay (Var "add" @@ [Val $ int 4, Val $ int 5])
                              ])
 
+fib :: Expr
+fib = lets [ ( [TermPat "sub" arith]
+             , (Val $ Prim "primSubInt32" arith)
+             )
+           , ( [TermPat "add" arith]
+             , (Val $ Prim "primAddInt32" arith)
+             )
+           , ( [TermPat "eq" arith]
+             , (Val $ Prim "primEqInt32" arith)
+             )
+           , ( [TermPat "or" arith]
+             , (Val $ Prim "primOrInt32" arith)
+             )
+           , fn "if" [ TermPat "p" int32
+                     , TypePat "a" Star
+                     , TermPat "x" (Lazy [TyVar "a"])
+                     , TermPat "y" (Lazy [TyVar "a"])
+                     ]
+                     [TyVar "a"]
+                     (Force $ primIf @@ [Var "p", Var "a", Var "x", Var "y"])
+           ]
+           (letrec
+              [ fun "fib"
+                  [ TermPat "n" $ int32]
+                  [int32]
+                  (Var "if"
+                      @@ [ Var "or" @@ [ Var "eq" @@ [Var "n", Val $ int 0]
+                                       , Var "eq" @@ [Var "n", Val $ int 1]
+                                       ]
+                         , Val $ Lit $ Type int32
+                         , Val $ Delay (Var "n")
+                         , Val $ Delay
+                             (Var "add"
+                                @@ [ Var "fib" @@ [Var "sub" @@ [Var "n", Val $ int 1]]
+                                   , Var "fib" @@ [Var "sub" @@ [Var "n", Val $ int 2]]
+                                   ]
+                             )
+                         ]
+                  )
+              ]
+              (Var "fib" @@ [Val $ int 25])
+           )
+
 main :: IO ()
-main = print test
+main = print (eval [] fib)
 
